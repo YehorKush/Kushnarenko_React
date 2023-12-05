5eaeebe21f7f6996c952ae17d19932d1595f2248
YehorKush <kushnarenkoegor@gmail.com>
2023-12-05 15:29:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from './components/Button/Button';
+import { Input } from "./components/Input/input";
+
+function App() {
+  return (
+    <div>
+      <Input></Input>
+      <Button></Button>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
